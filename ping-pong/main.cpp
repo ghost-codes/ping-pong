7e2d0df529be7ec564c78ca99df2c857db4aa823
Hope Dorkenoo <compounddork@gmail.com>
2023-12-08 17:29:53 +0000

    Initial Commit

---
+++ ping-pong/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ping-pong
+//
+//  Created by Nkatie3 on 08/12/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
